1142003cff2a390a79740a304bc825d095bb422f
Paula Di Cesare <paula.dicesare2@gmail.com>
2022-04-19 22:57:25 -0300

    clase-9

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { AppBar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import CartWidget from "./CartWidget";
+import { Wrapper, Logo, MenuItem, Left, Center, Right } from './styledComponents';
+import Search from "./Search";
+
+const NavBar = () => {
+    return (
+        <AppBar position="relative">
+            <Wrapper>
+                <Left>
+                    <Link to='/' style={{textDecoration: "none", color: "white"}}><Logo>MOOV</Logo></Link>
+                </Left>
+                <Center>
+                    <Link to='/category/9' style={{textDecoration: "none", color: "white"}}><MenuItem>Mujer</MenuItem></Link>
+                    <Link to='/category/1' style={{textDecoration: "none", color: "white"}}><MenuItem>Hombre</MenuItem></Link>
+                    <Link to='/category/5' style={{textDecoration: "none", color: "white"}}><MenuItem>Niños</MenuItem></Link> 
+                </Center>
+                <Search />
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem><CartWidget /></MenuItem>
+                </Right>
+            </Wrapper>
+        </AppBar>
+    );
+}
+
+export default NavBar;
---
+++ src/components/Search.js
@@ -0,0 +1,20 @@
+import { SearchContainer, Input } from './styledComponents';
+import SearchIcon from '@material-ui/icons/Search';
+
+const Search = () => {
+
+    const onInput = (e) => {
+        // e.preventDefault()
+        // console.log(e.key)
+        if (e.keyCode == 32) e.preventDefault()
+    }
+
+    return (
+        <SearchContainer>
+            <Input onKeyDown={onInput} />
+            <SearchIcon />
+        </SearchContainer>
+    );
+}
+
+export default Search;
